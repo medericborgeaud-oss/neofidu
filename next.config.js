fbbd30c7cd2faf9b726628bc2573e3c53819f8ef
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-03-24 11:33:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -126,7 +126,7 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https: blob:",
               "font-src 'self' data:",
