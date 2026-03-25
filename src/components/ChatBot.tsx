ef8d3cc2d5c2dc1e7a2768fec7006c7b1210fb55
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-03-25 22:00:44 +0100

    Update ChatBot.tsx

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -96,7 +96,7 @@ export function ChatBot() {
           </div>
 
           {/* Messages */}
-          <div className="flex-1 overflow-y-auto p-4 space-y-3 max-h-[380px] min-h-[200px]">
+          <div className="flex-1 overflow-y-auto p-4 space-y-3 min-h-0">
             {messages.map((msg, i) => (
               <div
                 key={i}
