c7f4fd62a967a132df95ccca095e2cc04c7e68dd
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-04-29 21:09:59 +0200

    Update companies.ts

--- src/lib/companies.ts
+++ src/lib/companies.ts
@@ -3,8 +3,11 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    fetch: (url, options = {}) => fetch(url, { ...options, cache: 'no-store' }),
+  },
+});
 export interface Company {
   id: string;
   slug: string;
