720639a6fd1efcb75063e816166354d399d5f45d
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-05-06 11:02:24 +0200

    Update communes.ts

--- src/lib/communes.ts
+++ src/lib/communes.ts
@@ -6,7 +6,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    fetch: (url: any, options: any = {}) =>
+      fetch(url, { ...options, cache: "no-store" }),
+  },
+});
 
 // ─── Types ───
 
