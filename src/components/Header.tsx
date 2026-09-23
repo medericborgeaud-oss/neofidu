1947ea1a03f23e2713b9573a1add91df56aaa270
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-09-23 15:55:48 +0200

    i18n: header maps /simulateur/3eme-pilier to /en

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,6 +21,7 @@ const EN_TWIN_EXACT = new Set<string>([
   "/expats",
   "/simulateur/impots",
   "/simulateur/salaire-net",
+  "/simulateur/3eme-pilier",
   "/independants",
   "/tarifs",
   "/entreprises",
