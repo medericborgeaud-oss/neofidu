d8d51d6dd1c91c3213b8a682b2a348da283f98f6
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-09-24 11:50:25 +0200

    i18n: footer maps /simulateur/valeur-locative to /en

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,6 +15,7 @@ const EN_TWIN_EXACT_FOOTER = new Set<string>([
   "/simulateur/impots",
   "/simulateur/salaire-net",
   "/simulateur/3eme-pilier",
+  "/simulateur/valeur-locative",
   "/independants",
   "/tarifs",
   "/entreprises",
