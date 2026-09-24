a6e06364c7730e11d987de80002ee47b839fd993
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-09-24 12:05:25 +0200

    i18n: footer maps /simulateur/baisse-loyer to /en

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,6 +17,7 @@ const EN_TWIN_EXACT_FOOTER = new Set<string>([
   "/simulateur/3eme-pilier",
   "/simulateur/valeur-locative",
   "/simulateur/gain-immobilier",
+  "/simulateur/baisse-loyer",
   "/independants",
   "/tarifs",
   "/entreprises",
