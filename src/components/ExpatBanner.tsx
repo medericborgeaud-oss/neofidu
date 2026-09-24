193a962251d43186c99f692fd3aabde4a207be3d
medericborgeaud-oss <medericborgeaud@gmail.com>
2026-09-24 11:51:18 +0200

    i18n: ExpatBanner maps /simulateur/valeur-locative to /en

--- src/components/ExpatBanner.tsx
+++ src/components/ExpatBanner.tsx
@@ -11,6 +11,7 @@ const EN_TWIN_EXACT = new Set<string>([
   "/simulateur/impots",
   "/simulateur/salaire-net",
   "/simulateur/3eme-pilier",
+  "/simulateur/valeur-locative",
   "/tarifs",
   "/entreprises",
   "/creation-entreprise",
